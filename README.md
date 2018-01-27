1b90ec80bb9adea9a36b62829a7124a02856063e
Zack Stout <zackstout@gmail.com>
2018-01-26 20:01:08 -0600

    added in machine learning

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Twitter Data Analysis
 
-Let's do some psychoanalysis! Following along with a few videos, but mostly inspired by this one: https://www.youtube.com/watch?v=o_OZdbCzHUA. There's no DOM, so we're just printing in the terminal and using MatPlot to generate some (DOM-less!) graphs.
+Let's do some psychoanalysis! Following along with a few videos, but mostly inspired by these two: https://www.youtube.com/watch?v=o_OZdbCzHUA and https://www.youtube.com/watch?v=si8zZHkufRY&t=444s. There's no DOM, so we're just printing in the terminal and using MatPlot to generate some (DOM-less!) graphs.
 
 ## Getting Started
 
 I had to use `pip3` instead of `pip`:
-`pip3 install tweepy
-pip3 install textblob`
+`pip3 install textblob`
 
-And then in order to use `textblob`, I had to run the following command: ```/Applications/Python\ 3.6/Install\ Certificates.command```
+And then in order to use `textblob`, I had to run the following command: `/Applications/Python\ 3.6/Install\ Certificates.command`
+
+Also important to `pip3 install scipy` and `pip3 install h5py`
--- index.py
+++ index.py
@@ -2,6 +2,16 @@
 import tweepy
 from textblob import TextBlob
 
+# import numpy as np
+# from sklearn.svm import SVR
+# import matplotlib.pyplot as plt
+
+import tflearn
+from tflearn.data_utils import to_categorical, pad_sequences
+from tflearn.datasets import imdb
+
+# plt.switch_backend('new_')
+
 consumer_key = 'BnlEfaOWDLUt9lHFO2CyMFXLZ'
 consumer_secret = 'sT0Gx7EGtu1uW7lll3AfhK6gOfzIT0OvWiT9b5JUXbzNn1I8KS'
 
@@ -15,7 +25,46 @@ api = tweepy.API(auth)
 
 public_tweets = api.search('Trump')
 
-for tweet in public_tweets:
-    print(tweet.text)
-    analysis = TextBlob(tweet.text)
-    print(analysis.sentiment)
+# for tweet in public_tweets:
+#     print(tweet.text)
+#     analysis = TextBlob(tweet.text)
+#     print(analysis.sentiment)
+
+
+
+train, test, _ = imdb.load_data(path='imdb.pkl', n_words=10000, valid_portion=0.1)
+trainX, trainY = train
+testX, testY = test
+
+trainX = pad_sequences(trainX, maxlen=100, value=0.)
+testX = pad_sequences(testX, maxlen=100, value=0.)
+
+trainY = to_categorical(trainY, nb_classes=2)
+testY = to_categorical(testY, nb_classes=2)
+
+net = tflearn.input_data([None, 100])
+net = tflearn.embedding(net, input_dim=10000, output_dim=128)
+#will randomly remove a neuron from the circuit on occasion:
+net = tflearn.lstm(net, 128, dropout=0.8)
+#softmax is doing the work of sigmoid
+net = tflearn.fully_connected(net, 2, activation='softmax')
+net = tflearn.regression(net, optimizer='adam', learning_rate=0.0001, loss='categorical_crossentropy')
+
+#this is training:
+model = tflearn.DNN(net, tensorboard_verbose=0)
+# oops looks like we need an = after validation_set
+model.fit(trainX, trainY, validation_set=(testX, testY), show_metric=True, batch_size=32)
+
+
+
+
+
+
+#Note, it's gonna take a minute or two or three to run the above code. Ok or way more. But it's doing the thing!! And loss is going down!!!!!!
+
+
+
+
+
+
+# print(train) wow that's a lot of numbers
